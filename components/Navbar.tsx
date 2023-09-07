88320e5b7719612bb7ead17c62da2bdd83f1ce42
Bilal Sheikh <72bilal.sheikh@gmail.com>
2023-09-07 21:02:13 +0530

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PcNavbar from './PcNavbar'
+import MobileNavbar from './MobileNavbar'
+import { currentUser } from "@clerk/nextjs";
+import RightSideNav from './RightSideNav';
+
+
+
+export default async function navbar() {
+
+    const user = await currentUser();
+
+    return (
+        <>
+            <header className="sticky top-0 bg-background border-b">
+                <div className="container flex h-16 items-center gap-4">
+
+                    <div className='block lg:hidden'>
+                        <MobileNavbar user={user} />
+                    </div>
+
+                    <div className='hidden lg:block '>
+                        <PcNavbar />
+                    </div>
+
+                    <RightSideNav user={user} />
+                </div>
+            </header>
+        </>
+    )
+}
