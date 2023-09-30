0156cd9510ebe7ab93ff866192708e41fd69fd0e
Bilal Sheikh <72bilal.sheikh@gmail.com>
2023-09-30 18:18:43 +0530

    added categories on landing page

--- components/RightSideNav.tsx
+++ components/RightSideNav.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, buttonVariants } from "./ui/button";
 import { ModeToggle } from "./ui/dark-mode";
-import { Search } from "lucide-react";
+import { Search, ShoppingCart } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { User } from "@clerk/nextjs/server";
@@ -119,18 +119,37 @@ export default function RightSideNav({ user, role }: NavbarProps) {
 							</DropdownMenuItem>
 						</DropdownMenuContent>
 					</DropdownMenu> */}
+
 					{user ? (
-						<UserButton afterSignOutUrl="/" />
+						<div className="flex items-center justify-center gap-3">
+							{role === "SELLER" ? (
+								<>
+									<UserButton afterSignOutUrl="/" />
+								</>
+							) : (
+								<>
+									<Button variant="outline" size="icon">
+										<ShoppingCart size={17} />
+									</Button>
+									<UserButton afterSignOutUrl="/" />
+								</>
+							)}
+						</div>
 					) : (
-						<Link
-							href={"/sign-in"}
-							className={buttonVariants({
-								variant: "default",
-							})}
-						>
-							{" "}
-							SignIn
-						</Link>
+						<div className="flex items-center justify-center gap-3">
+							<Button variant="outline" size="icon">
+								<ShoppingCart size={17} />
+							</Button>
+							<Link
+								href={"/sign-in"}
+								className={buttonVariants({
+									variant: "default",
+								})}
+							>
+								{" "}
+								SignIn
+							</Link>
+						</div>
 					)}
 				</div>
 			</div>
--- components/UserDashboard.tsx
+++ components/UserDashboard.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Navbar from "./Navbar";
 import axios from "axios";
+import clothing from "@/public/clothing.jpg";
+import accessories from "@/public/accessories.jpg";
+import electronics from "@/public/electronics.jpg";
+import mobiles from "@/public/mobiles.jpg";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { AspectRatio } from "./ui/aspect-ratio";
 
 export default async function UserDashboard() {
-
 	// works
 	// let prod
 	// try {
@@ -18,7 +24,7 @@ export default async function UserDashboard() {
 		<div>
 			<Navbar />
 			{/* <div>{JSON.stringify(prod)} works </div>  */}
-			
+
 			<section className="space-y-6 pb-16 pt-14 md:pb-24 md:pt-20 lg:py-40">
 				<div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
 					<h1 className="font-bold text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
@@ -37,38 +43,112 @@ export default async function UserDashboard() {
 			</section>
 
 			<section className="space-y-6 pb-16 pt-14 md:pb-24 md:pt-20 lg:py-36">
-				<div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+				<div className="container flex max-w-full flex-col items-center gap-4 text-center">
 					<h2 className="font-bold text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
 						Browse from Categories
 					</h2>
 					<p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
 						Find the products you're looking for in a variety of categories.
 					</p>
+
+					<div className="relative grid grid-cols-2 lg:grid-cols-4 justify-center rounded-xl gap-4">
+						<div className="overflow-hidden rounded-xl">
+							<div className="relative h-full">
+								<Image
+									src={clothing}
+									alt="clothing"
+									width={250}
+									height={330}
+									className="h-full transition-all hover:scale-105 rounded-xl"
+								/>
+								<div className="absolute flex top-0 left-0 bg-zinc-950/70 transition-colors hover:bg-zinc-950/75 text-white p-2 w-full h-20 justify-start">
+									<h2 className="scroll-m-20 pb-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0">
+										Clothing
+									</h2>
+								</div>
+							</div>
+						</div>
+						<div className="overflow-hidden rounded-xl">
+							<div className="relative h-full">
+								<Image
+									src={accessories}
+									alt="accessories"
+									width={250}
+									height={330}
+									className="h-full transition-all hover:scale-105 rounded-xl"
+								/>
+								<div className="absolute flex top-0 left-0 bg-zinc-950/70 transition-colors hover:bg-zinc-950/75 text-white p-2 w-full h-20 justify-start">
+									<h2 className="scroll-m-20 pb-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0">
+										Accessories
+									</h2>
+								</div>
+							</div>
+						</div>
+						<div className="overflow-hidden rounded-xl">
+							<div className="relative h-full">
+								<Image
+									src={electronics}
+									alt="electronics"
+									width={250}
+									height={330}
+									className="h-full transition-all hover:scale-105 rounded-xl"
+								/>
+								<div className="absolute flex top-0 left-0 bg-zinc-950/70 transition-colors hover:bg-zinc-950/75 text-white p-2 w-full h-20 justify-start">
+									<h2 className="scroll-m-20 pb-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0">
+										Electronics
+									</h2>
+								</div>
+							</div>
+						</div>
+						<div className="overflow-hidden rounded-xl">
+							<div className="relative h-full">
+								<Image
+									src={mobiles}
+									alt="mobiles"
+									width={250}
+									height={330}
+									className="h-full transition-all hover:scale-105 rounded-xl"
+								/>
+								<div className="absolute flex top-0 left-0 bg-zinc-950/70 transition-colors hover:bg-zinc-950/75 text-white p-2 w-full h-20 justify-start">
+									<h2 className="scroll-m-20 pb-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0">
+										Mobiles
+									</h2>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</section>
 
-			<div>
-				<section className="space-y-6 pb-16 pt-14 md:pb-24 md:pt-20 lg:py-36">
-					<div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-						<h2 className="font-bold text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
-							Browse from Categories
-						</h2>
-						<p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-							Find the products you're looking for in a variety of categories.
-						</p>
-					</div>
-				</section>
-				<section className="space-y-6 pb-16 pt-14 md:pb-24 md:pt-20 lg:py-36">
-					<div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-						<h2 className="font-bold text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
-							Browse from Categories
-						</h2>
-						<p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-							Find the products you're looking for in a variety of categories.
-						</p>
-					</div>
-				</section>
-			</div>
+			<section className="space-y-6 pb-16 pt-14 md:pb-24 md:pt-20 lg:py-36">
+				<div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+					<h2 className="font-bold text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+						Top Sellers
+					</h2>
+					<p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+						Here are the products top 4 best selling products for this month
+					</p>
+				</div>
+			</section>
+
+			<section className="space-y-6 pb-16 pt-14 md:pb-24 md:pt-20 lg:py-36">
+				<div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+					<h2 className="font-bold text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+						Value for Money Picks
+					</h2>
+					<p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+						Explore our curated collection of budget-friendly items that deliver
+						quality without breaking the bank
+					</p>
+
+
+
+
+
+
+
+				</div>
+			</section>
 		</div>
 	);
 }
