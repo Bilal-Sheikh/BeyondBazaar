da8655b3c569d3537b4de779ba2878e6509987e2
Bilal Sheikh <72bilal.sheikh@gmail.com>
2023-10-14 17:03:33 +0530

    test

--- app/(seller)/view-shop/page.tsx
+++ app/(seller)/view-shop/page.tsx
@@ -166,7 +166,7 @@ export default async function DashboardPage() {
 									</Tooltip>
 								</TooltipProvider>
 							</CardTitle>
-							<svg
+							{/* <svg
 								xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 24 24"
 								fill="none"
@@ -177,7 +177,7 @@ export default async function DashboardPage() {
 								className="h-4 w-4 text-muted-foreground"
 							>
 								<path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-							</svg>
+							</svg> */}
 						</CardHeader>
 						<CardContent>
 							<div className="text-2xl font-bold">${totalRevenue}</div>
@@ -209,7 +209,7 @@ export default async function DashboardPage() {
 									</Tooltip>
 								</TooltipProvider>
 							</CardTitle>
-							<svg
+							{/* <svg
 								xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 24 24"
 								fill="none"
@@ -222,7 +222,7 @@ export default async function DashboardPage() {
 								<path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
 								<circle cx="9" cy="7" r="4" />
 								<path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-							</svg>
+							</svg> */}
 						</CardHeader>
 						<CardContent>
 							<div className="text-2xl font-bold">{totalInCarts}</div>
@@ -253,7 +253,7 @@ export default async function DashboardPage() {
 									</Tooltip>
 								</TooltipProvider>
 							</CardTitle>
-							<svg
+							{/* <svg
 								xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 24 24"
 								fill="none"
@@ -265,7 +265,7 @@ export default async function DashboardPage() {
 							>
 								<rect width="20" height="14" x="2" y="5" rx="2" />
 								<path d="M2 10h20" />
-							</svg>
+							</svg> */}
 						</CardHeader>
 						<CardContent>
 							<div className="text-2xl font-bold">{totalSales}</div>
